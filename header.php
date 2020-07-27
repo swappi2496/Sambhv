5f9b36cce0148cb646b2cf95d266a6208f6b898f
swappi2496 <agrawalswappi96@gmail.com>
2020-07-28 01:00:42 +0530

    My-Project

---
+++ header.php
@@ -0,0 +1,44 @@
+ <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+
+            <div class="container-fluid col-sm-12 ml-3 mr-3" style="margin-top: 50px; margin-bottom: 20px;">
+      <img class="img-fluid logo" src="images/Group 3.jpg"><img>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light" style="float:right">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a style="font-size: 18px; font-family: raleway" class="nav-link hvr-bounce-in" href="index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a style="font-size: 18px; font-family: raleway" style="font-size: 18px; font-family: raleway" class="nav-link hvr-bounce-in" href="about_us.html">About us</a>
+            </li>
+            <li class="nav-item">
+              <a style="font-size: 18px; font-family: raleway" class="nav-link hvr-bounce-in" href="product.html">Product</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a style="font-size: 18px; font-family: raleway" class="nav-link dropdown-toggle" href="corporate_sustainability.html" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Corporated Sustainability </a>
+                
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    
+                <a style="font-size: 18px; font-family: raleway" href="environment.html" class="nav-link">Environment<span class="sr-only">(current)</span></a>
+              <a  style="font-size: 18px; font-family: raleway" href="certificate.html" class="nav-link">Certificate</a>
+              <a style="font-size: 18px; font-family: raleway" href="soc_res.html" class="nav-link">Social Responsibility</a>
+                </div>
+            </li>
+            <li class="nav-item">
+              <a style="font-size: 18px; font-family: raleway" class="nav-link hvr-bounce-in" href="career.html">Career</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    
+          </div>
+        </div>
+      </div>
